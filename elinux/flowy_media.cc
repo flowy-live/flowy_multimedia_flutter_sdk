b18ccc69b9957ba08bd15f9ee48932050d39276b
talksik <patel.arjun50@gmail.com>
2023-09-17 09:24:37 -0700

    fixing bugs

--- elinux/flowy_media.cc
+++ elinux/flowy_media.cc
@@ -39,10 +39,12 @@ void FlowyMedia::Init()
     std::cout << "Starting receive audio pipeline" << std::endl;
     gst_element_set_state(m_audio_receive_pipeline, GST_STATE_PLAYING);
 
+    // TODO: may need to fix based on camera device on embedded
+    // TODO: use opus maybe for audio here
     m_record_pipeline = gst_parse_launch(
-        "v4l2src ! videoconvert ! queue ! x264enc tune=zerolatency ! "
-        "mux. alsasrc ! queue ! audioconvert ! audioresample ! voaacenc ! "
-        "aacparse ! qtmux name=mux ! filesink location=test.mp4 sync=false",
+        "gst-launch-1.0 -e v4l2src ! video/x-raw,width=640,height=480,framerate=30/1 ! "
+        "videoconvert ! x264enc tune=zerolatency ! mux. alsasrc ! queue ! audioconvert ! "
+        "audioresample ! voaacenc ! aacparse ! qtmux name=mux ! filesink location=test.mp4 sync=false",
         NULL);
     gst_element_set_state(m_record_pipeline, GST_STATE_PAUSED);
 }
@@ -97,7 +99,7 @@ void FlowyMedia::StartRecord()
 std::string FlowyMedia::StopRecord()
 {
     std::cout << "Stopping record" << std::endl;
-    gst_element_set_state(m_record_pipeline, GST_STATE_PAUSED);
+    gst_element_set_state(m_record_pipeline, GST_STATE_NULL);
 
     // get full path to file
     return std::string("./test.mp4");
