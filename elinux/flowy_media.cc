721a8d7075329467829e31b0aafc82b2f052dd5a
talksik <patel.arjun50@gmail.com>
2023-09-18 12:33:17 -0700

    making video with test audio work

--- elinux/flowy_media.cc
+++ elinux/flowy_media.cc
@@ -41,18 +41,24 @@ void FlowyMedia::Init()
 
     // TODO: may need to fix based on camera device on embedded
     // TODO: use opus maybe for audio here
-    GstElement* audio_src   = gst_element_factory_make("audiotestsrc", "audio-source");
-    GstElement* video_src   = gst_element_factory_make("videotestsrc", "video-source");
-    GstElement* audio_enc   = gst_element_factory_make("voaacenc", "audio-enc");
-    GstElement* video_enc   = gst_element_factory_make("x264enc", "video-enc");
-    GstElement* audio_queue = gst_element_factory_make("queue", "audio-queue");
-    GstElement* mux         = gst_element_factory_make("avimux", "record-mux");
-    GstElement* file_sink   = gst_element_factory_make("filesink", "record-sink");
+    GstElement* audio_src     = gst_element_factory_make("audiotestsrc", "audio-source");
+    GstElement* video_src     = gst_element_factory_make("v4l2src", "video-source");
+    GstElement* video_convert = gst_element_factory_make("videoconvert", "video-convert");
+    GstElement* audio_enc     = gst_element_factory_make("voaacenc", "audio-enc");
+    GstElement* video_enc     = gst_element_factory_make("x264enc", "video-enc");
+    GstElement* audio_queue   = gst_element_factory_make("queue", "audio-queue");
+    GstElement* mux           = gst_element_factory_make("avimux", "record-mux");
+    GstElement* file_sink     = gst_element_factory_make("filesink", "record-sink");
+
+    // set caps for video src
+    GstCaps* video_caps = gst_caps_from_string("video/x-raw,width=640,height=480,framerate=30/1");
+    GstElement* video_filter = gst_element_factory_make("capsfilter", "video-filter");
+    g_object_set(G_OBJECT(video_filter), "caps", video_caps, NULL);
 
     m_record_pipeline = gst_pipeline_new("record-pipeline");
 
-    if (!audio_src || !video_src || !audio_enc || !video_enc || !audio_queue || !mux || !file_sink
-        || !m_record_pipeline)
+    if (!audio_src || !video_src || !video_convert || !audio_enc || !video_enc || !audio_queue
+        || !mux || !file_sink || !m_record_pipeline)
     {
         std::cerr << "MEDIA: failed to create elements" << std::endl;
         return;
@@ -60,6 +66,8 @@ void FlowyMedia::Init()
 
     gst_bin_add_many(GST_BIN(m_record_pipeline),
                      video_src,
+                     video_filter,
+                     video_convert,
                      audio_src,
                      audio_queue,
                      audio_enc,
@@ -72,7 +80,8 @@ void FlowyMedia::Init()
         g_printerr("Error(audio): Element could't be lined.\n");
         return;
     }
-    if (!gst_element_link_many(video_src, video_enc, mux, file_sink, NULL))
+    if (!gst_element_link_many(
+            video_src, video_filter, video_convert, video_enc, mux, file_sink, NULL))
     {
         g_printerr("Error(video): Element could't be lined.\n");
         return;
