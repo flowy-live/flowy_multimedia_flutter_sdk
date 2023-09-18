c47bea06e77046229599f6c5413f6885b86ed5d6
talksik <patel.arjun50@gmail.com>
2023-09-18 11:56:10 -0700

    creating manually and working with audio and video test

--- elinux/flowy_media.cc
+++ elinux/flowy_media.cc
@@ -41,10 +41,44 @@ void FlowyMedia::Init()
 
     // TODO: may need to fix based on camera device on embedded
     // TODO: use opus maybe for audio here
-    m_record_pipeline = gst_parse_launch(
-        "videotestsrc ! x264enc ! mp4mux name=mux ! filesink location=test.mp4  audiotestsrc ! lamemp3enc ! mux.",
-        NULL);
-    gst_element_set_state(m_record_pipeline, GST_STATE_PAUSED);
+    GstElement* audio_src   = gst_element_factory_make("audiotestsrc", "audio-source");
+    GstElement* video_src   = gst_element_factory_make("videotestsrc", "video-source");
+    GstElement* audio_enc   = gst_element_factory_make("voaacenc", "audio-enc");
+    GstElement* video_enc   = gst_element_factory_make("x264enc", "video-enc");
+    GstElement* audio_queue = gst_element_factory_make("queue", "audio-queue");
+    GstElement* mux         = gst_element_factory_make("avimux", "record-mux");
+    GstElement* file_sink   = gst_element_factory_make("filesink", "record-sink");
+
+    m_record_pipeline = gst_pipeline_new("record-pipeline");
+
+    if (!audio_src || !video_src || !audio_enc || !video_enc || !audio_queue || !mux || !file_sink
+        || !m_record_pipeline)
+    {
+        std::cerr << "MEDIA: failed to create elements" << std::endl;
+        return;
+    }
+
+    gst_bin_add_many(GST_BIN(m_record_pipeline),
+                     video_src,
+                     audio_src,
+                     audio_queue,
+                     audio_enc,
+                     video_enc,
+                     mux,
+                     file_sink,
+                     NULL);
+    if (!gst_element_link_many(audio_src, audio_enc, audio_queue, mux, NULL))
+    {
+        g_printerr("Error(audio): Element could't be lined.\n");
+        return;
+    }
+    if (!gst_element_link_many(video_src, video_enc, mux, file_sink, NULL))
+    {
+        g_printerr("Error(video): Element could't be lined.\n");
+        return;
+    }
+
+    g_object_set(file_sink, "location", "test.mp4", NULL);
 }
 
 FlowyMedia::~FlowyMedia()
