4bc05ae198adbb641ca7e3fe20af5457b7fcd22d
talksik <patel.arjun50@gmail.com>
2023-10-06 11:53:55 -0700

    making plugin work with pubspec change

--- example/linux/flutter/generated_plugin_registrant.cc
+++ example/linux/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,10 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <flowy_multimedia/flowy_multimedia_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) flowy_multimedia_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlowyMultimediaPlugin");
+  flowy_multimedia_plugin_register_with_registrar(flowy_multimedia_registrar);
 }
--- linux/flowy_multimedia_plugin.cc
+++ linux/flowy_multimedia_plugin.cc
@@ -4,6 +4,8 @@
 #include <gtk/gtk.h>
 #include <sys/utsname.h>
 
+#include <iostream>
+
 #include <cstring>
 
 #include "flowy_multimedia_plugin_private.h"
@@ -32,6 +34,7 @@ static void flowy_multimedia_plugin_handle_method_call(
     response = FL_METHOD_RESPONSE(fl_method_not_implemented_response_new());
   }
 
+  // response = get_platform_version();
   fl_method_call_respond(method_call, response, nullptr);
 }
 
