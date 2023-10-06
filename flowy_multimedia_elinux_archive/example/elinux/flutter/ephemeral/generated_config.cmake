081c2420ebb3659f118f1bb97f8523b8a881d67b
talksik <patel.arjun50@gmail.com>
2023-10-06 11:05:02 -0700

    boom

---
+++ flowy_multimedia_elinux_archive/example/elinux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,20 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/talksik/code/flutter-elinux/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/talksik/code/flowy_multimedia/example" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/talksik/code/flutter-elinux/flutter"
+  "PROJECT_DIR=/home/talksik/code/flowy_multimedia/example"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/talksik/code/flowy_multimedia/example/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=lib/main.dart"
+)
